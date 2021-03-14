af203c5925c9b60759162ec32720a9d8f93dc1c5
paolapog <ppoggini@lifeed.io>
2021-03-14 08:07:20 +0100

    use state greetings

---
+++ react-hooks/1.useStateGreetings/01.js
@@ -0,0 +1,30 @@
+// useState: greeting
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+// EX + EXTRA-1
+
+function Greeting({initialName}) {
+  const [name, setName] = React.useState(initialName);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
