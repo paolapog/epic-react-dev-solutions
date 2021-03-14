bcdb6c0493c0f6778465a561558f93a8e6415f7a
paolapog <ppoggini@lifeed.io>
2021-03-14 07:36:57 +0100

    remove comment

--- react-fundamentals/7.Arrays/07.js
+++ react-fundamentals/7.Arrays/07.js
@@ -31,7 +31,6 @@ function App() {
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
           <li key={`${item.id}-${item.value}`}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
